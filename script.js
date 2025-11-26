1f893726e001cc5e1bf27743b1c7f9131dc6d518
omarfive15 <omarfive546@gmail.com>
2025-11-26 12:57:12 +0100

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-console.log("Parallel World Loaded Successfully!");
-const enterButton = document.getElementById('enterWorld');
-const background = document.querySelector('.background');
-if(enterButton){enterButton.addEventListener('click',()=>{background.style.filter='brightness(0.5) hue-rotate(120deg)'; enterButton.innerText="Welcome to the Parallel World!";});}
-const characterImages = document.querySelectorAll('.character-card img');
-characterImages.forEach(img=>{img.addEventListener('mouseenter',()=>{const audio=new Audio('assets/effect.mp3'); audio.play();});});
